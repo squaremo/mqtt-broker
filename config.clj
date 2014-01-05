d77b7345fd4075125b49f9f195ce4cc51d6ded75
Malcolm Sparks <malcolm@congreve.com>
2014-01-05 02:55:26 +0000

    Got simple PUBLISH working

--- config.clj
+++ config.clj
@@ -5,11 +5,15 @@
   {:jig/component mqtt-broker.core/MqttDecoder
    :jig/project "../mqtt-broker/project.clj"}
 
+  :encoder
+  {:jig/component mqtt-broker.core/MqttEncoder
+   :jig/project "../mqtt-broker/project.clj"}
+
   :handler
   {:jig/component mqtt-broker.core/MqttHandler
    :jig/project "../mqtt-broker/project.clj"}
 
   :server
   {:jig/component mqtt-broker.core/Server
-   :jig/dependencies [:decoder :handler]
+   :jig/dependencies [:decoder :encoder :handler]
    :jig/project "../mqtt-broker/project.clj"}}}
--- src/mqtt_broker/core.clj
+++ src/mqtt_broker/core.clj
@@ -3,6 +3,7 @@
    jig
    [jig.util :refer (get-dependencies satisfying-dependency)]
    [mqtt.decoder :refer (make-decoder)]
+   [mqtt.encoder :refer (make-encoder)]
    [clojure.tools.logging :refer :all])
   (:import
    (io.netty.util ReferenceCountUtil)
@@ -16,45 +17,56 @@
   Lifecycle
   (init [_ system] system)
   (start [_ system]
-    (infof "Adding netty handler to system at %s" [(:jig/id config) ::handler])
-    (assoc-in system [(:jig/id config) ::handler]
-              (make-decoder)))
+    (debugf "Adding netty handler to system at %s" [(:jig/id config) ::handler-factory])
+    (assoc-in system [(:jig/id config) ::handler-factory] #(make-decoder)))
   (stop [_ system] system))
+
+(deftype MqttEncoder [config]
+  Lifecycle
+  (init [_ system] system)
+  (start [_ system]
+    (debugf "Adding netty handler to system at %s" [(:jig/id config) ::handler-factory])
+    (assoc-in system [(:jig/id config) ::handler-factory] #(make-encoder)))
+  (stop [_ system] system))
+
+(defn reply [ctx msg]
+  (doto ctx
+    (.write msg)
+    (.flush)))
 
 (deftype MqttHandler [config]
   Lifecycle
   (init [_ system] system)
   (start [_ system]
-    (infof "Adding netty handler to system at %s" [(:jig/id config) ::handler])
-    (assoc-in system [(:jig/id config) ::handler]
-              (proxy [ChannelHandlerAdapter] []
-                (channelRead [ctx msg]
-                  (infof "Got request!! msg is %s" msg)
-                  (.write ctx msg)
-                  (.flush ctx)
-                  ;;(.release msg)
-                  )
-                (exceptionCaught [ctx cause]
-                  (.printStackTrace cause)
-                  (.close ctx)))))
+    (debugf "Adding netty handler to system at %s" [(:jig/id config) ::handler-factory])
+    (assoc-in
+     system
+     [(:jig/id config) ::handler-factory]
+     #(proxy [ChannelHandlerAdapter] []
+        (channelRead [ctx msg]
+          (case (:type msg)
+            :connect (reply ctx {:type :connack})
+            :publish (infof "PUBLISH MESSAGE: topic is %s, payload is '%s'" (:topic msg) (String. (:payload msg)))
+            :disconnect (.close ctx)
+            (throw (ex-info (format "TODO: handle type: %s" (:type msg)) msg))))
+
+        (exceptionCaught [ctx cause]
+          (.printStackTrace cause)
+          (.close ctx)))))
   (stop [_ system] system))
 
-(deftype GenericHandler [config]
+(deftype EchoHandler [config]
   Lifecycle
   (init [_ system] system)
   (start [_ system]
-    (infof "Adding netty handler to system at %s" [(:jig/id config) ::handler])
-    (assoc-in system [(:jig/id config) ::handler]
-           (proxy [ChannelHandlerAdapter] []
-             (channelRead [ctx msg]
-               (infof "Got request!! msg is %s" (type msg))
-               (.write ctx msg)
-               (.flush ctx)
-               ;;(.release msg)
-               )
-             (exceptionCaught [ctx cause]
-               (.printStackTrace cause)
-               (.close ctx)))))
+    (assoc-in system [(:jig/id config) ::handler-factory]
+              #(proxy [ChannelHandlerAdapter] []
+                 (channelRead [ctx msg]
+                   (.write ctx msg)
+                   (.flush ctx))
+                 (exceptionCaught [ctx cause]
+                   (.printStackTrace cause)
+                   (.close ctx)))))
   (stop [_ system] system))
 
 ;; From https://github.com/netty/netty/wiki/User-guide-for-5.x
@@ -63,13 +75,13 @@
   (init [_ system] system)
   (start [_ system]
 
-    (infof "Dependencies are %s" (vec (get-dependencies system config)))
+    (debugf "Dependencies are %s" (vec (get-dependencies system config)))
 
-    (let [handlers (keep #(get-in system [(:jig/id %) ::handler]) (get-dependencies system config))]
+    (let [handlers (keep #(get-in system [(:jig/id %) ::handler-factory]) (get-dependencies system config))]
 
       (when (empty? handlers)
         (throw (ex-info (format "No dependencies of %s register entries of %s"
-                                (:jig/id config) ::handler) {:jig/id (:jig/id config)})))
+                                (:jig/id config) ::handler-factory) {:jig/id (:jig/id config)})))
 
       (let [boss-group (NioEventLoopGroup.)
             worker-group (NioEventLoopGroup.)]
@@ -80,8 +92,8 @@
               (.childHandler
                (proxy [ChannelInitializer] []
                  (initChannel [ch]
-                   (infof "Initializing channel with handlers: %s" (vec handlers))
-                   (-> ch (.pipeline) (.addLast (into-array ChannelHandler handlers))))))
+                   (debugf "Initializing channel with handlers: %s" (vec handlers))
+                   (-> ch (.pipeline) (.addLast (into-array ChannelHandler (map (fn [f] (f)) handlers)))))))
               (.option ChannelOption/SO_BACKLOG (int 128))
               (.childOption ChannelOption/SO_KEEPALIVE true))
 
