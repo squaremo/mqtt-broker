d56b25f6e7682015a91fd316de5d784a57ae0e66
Malcolm Sparks <malcolm@congreve.com>
2014-01-10 18:55:39 +0000

    Remove 'factory' from code language, this isn't GoF.

--- src/mqtt_broker/core.clj
+++ src/mqtt_broker/core.clj
@@ -5,23 +5,22 @@
    (io.netty.channel ChannelHandlerAdapter)
    (jig Lifecycle)))
 
-(defn make-channel-handler-factory [subs]
-  (fn []
-    (proxy [ChannelHandlerAdapter] []
-      (channelRead [ctx msg]
-        (case (:type msg)
-          :connect (.writeAndFlush ctx {:type :connack})
-          :subscribe (do (.writeAndFlush ctx {:type :suback})
-                         (swap! subs (fn [subs]
-                                       (reduce #(update-in %1 [%2] conj ctx)
-                                               subs (map first (:topics msg))))))
-          :publish (doseq [ctx (get @subs (:topic msg))]
-                     (.writeAndFlush ctx msg))
-          :pingreq (.writeAndFlush ctx {:type :pingresp})
-          :disconnect (.close ctx)))
-      (exceptionCaught [ctx cause]
-        (try (throw cause)
-             (finally (.close ctx)))))))
+(defn make-channel-handler [subs]
+  (proxy [ChannelHandlerAdapter] []
+    (channelRead [ctx msg]
+      (case (:type msg)
+        :connect (.writeAndFlush ctx {:type :connack})
+        :subscribe (do (.writeAndFlush ctx {:type :suback})
+                       (swap! subs (fn [subs]
+                                     (reduce #(update-in %1 [%2] conj ctx)
+                                             subs (map first (:topics msg))))))
+        :publish (doseq [ctx (get @subs (:topic msg))]
+                   (.writeAndFlush ctx msg))
+        :pingreq (.writeAndFlush ctx {:type :pingresp})
+        :disconnect (.close ctx)))
+    (exceptionCaught [ctx cause]
+      (try (throw cause)
+           (finally (.close ctx))))))
 
 (deftype MqttHandler [config]
   Lifecycle
@@ -29,5 +28,5 @@
   (start [_ system]
     (assoc-in system
               [(:jig/id config) :jig.netty/handler-factory]
-              (make-channel-handler-factory (atom {}))))
+              #(make-channel-handler (atom {}))))
   (stop [_ system] system))
